179f6cb70db3a2cf9c4ef206ab25791c5a1f7df8
ProfNpc <professornpc@gmail.com>
2020-02-10 14:24:38 -0300

    Implementação de publicação de recados

---
+++ SistemaRecados/Controllers/RecadoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaRecados.Models;
+
+namespace SistemaRecados.Controllers
+{
+    public class RecadoController : Controller
+    {
+        private static IList<Recado> recados = new List<Recado>();
+
+
+        public IActionResult Index()
+        {
+
+            return View(recados.OrderByDescending(r => r.Horario));
+        }
+
+        public ActionResult NovoRecado()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NovoRecado(Recado recado)
+        {
+            if (recado.Autor == null)
+            {
+                recado.Autor = "Anônimo";
+            }
+            recado.Horario = DateTime.Now;
+            recado.Mensagem = recado.Mensagem.Trim();
+            recados.Add(recado);
+            recado.RecadoID = recados.Select(r => r.RecadoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SistemaRecados/Models/Recado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaRecados.Models
+{
+    public class Recado
+    {
+        public long? RecadoID { get; set; }
+        public string Mensagem { get; set; }
+        public string Autor { get; set; }
+        public DateTime Horario { get; set; }
+    }
+}
